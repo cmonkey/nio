4830124bd6ec5b973192a5b736a661503a49ba54
CMonkey <42.codemonkey@gmail.com>
2019-10-23 15:45:06 +0800

    add read connect msg to NioClient

--- src/main/scala/org/excavator/boot/nio/NioClient.scala
+++ src/main/scala/org/excavator/boot/nio/NioClient.scala
@@ -1,15 +1,27 @@
 package org.excavator.boot.nio
 
 import java.net.InetSocketAddress
+import java.nio.ByteBuffer
 import java.nio.channels.SocketChannel
 import java.nio.charset.StandardCharsets
 import java.util.Scanner
 
 import org.apache.commons.lang3.StringUtils
+import org.slf4j.LoggerFactory
 
 class NioClient(port: Int) {
+  val logger = LoggerFactory.getLogger(classOf[NioClient])
   def connect(): Unit = {
     val socketChannel = SocketChannel.open(new InetSocketAddress(port))
+
+    val byteBuffer = ByteBuffer.allocate(1024)
+
+    socketChannel.read(byteBuffer)
+
+    while(byteBuffer.hasRemaining){
+      byteBuffer.flip()
+      logger.info(s"read connection info ${StandardCharsets.UTF_8.decode(byteBuffer)}")
+    }
 
     val scanner = new Scanner(System.in)
 
